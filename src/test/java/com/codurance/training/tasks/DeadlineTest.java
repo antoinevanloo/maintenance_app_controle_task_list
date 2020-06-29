6f994b5933c33cd2c7157e4b0dd8a983335b4165
Antoine VAN LOO <vanlooantoine@gmail.com>
2020-06-29 12:02:48 +0200

    update type LocalDateTime to LocalDate

--- src/test/java/com/codurance/training/tasks/DeadlineTest.java
+++ src/test/java/com/codurance/training/tasks/DeadlineTest.java
@@ -2,7 +2,7 @@ package com.codurance.training.tasks;
 
 import org.junit.Test;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,16 +16,16 @@ public class DeadlineTest {
 
     @Test
     public void check_date_is_correct_test(){
-        LocalDateTime datetime = LocalDateTime.of(2017, 1, 14, 10, 34);
+        LocalDate datetime = LocalDate.of(2017, 1, 14);
         Deadline uneDeadline = new Deadline(42,datetime);
 
-        assertEquals(LocalDateTime.of(2017, 1, 14, 10, 34),uneDeadline.getDate());
+        assertEquals(LocalDate.of(2017, 1, 14),uneDeadline.getDate());
     }
 
     @Test
     public void check_date_is_today_test(){
         Deadline uneDeadline = new Deadline(42);
-        LocalDateTime dateTime = LocalDateTime.now();
+        LocalDate dateTime = LocalDate.now();
         uneDeadline.setDate(dateTime);
 
         assertEquals(true, uneDeadline.isDateToday());
