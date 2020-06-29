dcaaacc84ba376781f9c1fe4b92666362a590b2d
Antoine VAN LOO <vanlooantoine@gmail.com>
2020-06-29 10:12:26 +0200

    add TaskTest for Description is correct

--- src/test/java/com/codurance/training/tasks/TaskTest.java
+++ src/test/java/com/codurance/training/tasks/TaskTest.java
@@ -7,9 +7,13 @@ public class TaskTest {
 
     @Test
     public void check_id_is_correct_test(){
-
         Task uneTask = new Task(42,"ma petite description", true);
-
         assertEquals(42,uneTask.getId());
+    }
+
+    @Test
+    public void check_if_description_is_correct(){
+        Task uneTask = new Task(1,"T'es mauvais Jack", false);
+        assertEquals("T'es mauvais Jack",uneTask.getDescription());
     }
 }
